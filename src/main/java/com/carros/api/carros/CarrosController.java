f7618ac655cac15e1a8b5cd41d7d508eac198180
Sofia Queiroz <ouniversodesofiawolf@gmail.com>
2024-12-17 14:03:02 -0300

    inicializando processo de add comandos de status do http
    Junto a isso apagando os comentários menos necessários no momento, para que não fique muito comentário no codigo. Então ficará sempre a mostra os comentários a respeito dos novos assuntos quando necessário.

--- src/main/java/com/carros/api/carros/CarrosController.java
+++ src/main/java/com/carros/api/carros/CarrosController.java
@@ -3,6 +3,8 @@ package com.carros.api.carros;
 import com.carros.domain.Carro;
 import com.carros.domain.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -15,20 +17,16 @@ public class CarrosController {
     private CarroService servise; // não precisa mais criar um new objeto pq o @Autowired vai faer isso por si so
     // isso é o conceito de DI = INJEÇÃO DE DEPENDENCIAS
 
-    @GetMapping
-    public Iterable<Carro> getListacarros() {
-        return servise.getCarros();
+    @GetMapping // esse metodo não precisaria da aplicação dos status de retorno do http pois ele deve voltar 200 que é o numero padrão mas faremos o codigo em todos
+    public ResponseEntity<Iterable<Carro>> getListacarros() {
+        return new ResponseEntity<>(servise.getCarros(), HttpStatus.OK); // aqui vai chavar o servise que precisamos e caso finalizado retorna o status correspoendente do comando http (200)
     }
-    // INTERABLE é uma interface que representa uma coleção que pode ser listada.
-    // permite acessar os elementos de uma coleção um a um sem expor a implementação interna deles
+
 
     @GetMapping ("/{id}")
     public Optional<Carro> get(@PathVariable("id") Long id){ return servise.getCarroById(id);}
-    // OPTIONAL também é uma interface. é uma classe conteiner que representa um valor que pode ou não estar presente
-    // é usado quando pode retornar null (o valor pode ou não existir)
 
-//  @GetMapping("tipo/{tipo}")
-//  public Iterable<Carro> get(@PathVariable("tipo") String tipo){return servise.getCarroByTipo(tipo);}
+
 
     @PostMapping // se fizer um post no /api/v1/cavalos passando um JSON de cavalo como parametro, ele vai executar esse metodo:
     public String post(@RequestBody Carro carro){//@RequestBody converte o JSON do cavalo para o objeto cavalo
