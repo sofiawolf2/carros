77148c6a90eb3a4fcb6902cf87d1d9a5a4294023
sofiawolf2 <ouniversodesofiawolf@gmail.com>
2025-01-09 11:39:10 -0300

    add jwt

--- src/main/java/br/com/taurus/projeto/curso/carros/security/SecurityConfig.java
+++ src/main/java/br/com/taurus/projeto/curso/carros/security/SecurityConfig.java
@@ -1,13 +1,20 @@
 package br.com.taurus.projeto.curso.carros.security;
 
+import br.com.taurus.projeto.curso.carros.security.jwt.JwtAuthenticationFilter;
+import br.com.taurus.projeto.curso.carros.security.jwt.JwtAuthorizationFilter;
+import br.com.taurus.projeto.curso.carros.security.jwt.handler.AccessDeniedHandler;
+import br.com.taurus.projeto.curso.carros.security.jwt.handler.UnauthorizedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,13 +25,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired @Qualifier ("userDetailsService") // usado para definir qual classe estamos usando em vez de definitir UserDetailsServiceImp
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private UnauthorizedHandler unauthorizedHandler;
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /*
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().csrf().disable(); //retira a requisição de autenticação de chamadas
+
+         */
+        AuthenticationManager authManager = authenticationManager();
+
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/login").permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .addFilter(new JwtAuthenticationFilter(authManager))
+                .addFilter(new JwtAuthorizationFilter(authManager, userDetailsService))
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
     }
 
 
